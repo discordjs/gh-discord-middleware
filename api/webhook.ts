cab5cde92b4119bd59710925a5dd706e9b99bb0b
ckohen <chaikohen@gmail.com>
2022-07-27 07:01:13 -0700

    fix: only send githubs headers

--- api/webhook.ts
+++ api/webhook.ts
@@ -37,9 +37,25 @@ async function rewrite(req: VercelRequest, res: VercelResponse, target: Exclude<
 
 	try {
 		const body = `${JSON.stringify(req.body, null, 2)}\n`;
-		const discordRes = await fetch(url, { body, headers: req.headers as Record<string, string>, method: req.method });
-		const headers = [...discordRes.headers];
-		res.writeHead(discordRes.status, discordRes.statusText, headers);
+		const headers: Record<string, string> = {};
+		if (req.headers.accept) headers.Accept = req.headers.accept;
+		if (req.headers['content-type']) headers['Content-Type'] = req.headers['content-type'];
+		if (req.headers['user-agent']) headers['User-Agent'] = req.headers['user-agent'];
+		if (req.headers['x-github-delivery']) headers['X-Github-Delivery'] = req.headers['x-github-delivery'] as string;
+		if (req.headers['x-gitHub-event']) headers['X-GitHub-Event'] = req.headers['x-gitHub-event'] as string;
+		if (req.headers['x-github-hook-id']) headers['X-GitHub-Hook-ID'] = req.headers['x-github-hook-id'] as string;
+		if (req.headers['x-github-hook-installation-target-id']) {
+			headers['X-GitHub-Hook-Installation-Target-ID'] = req.headers['x-github-hook-installation-target-id'] as string;
+		}
+		if (req.headers['x-github-hook-installation-target-type']) {
+			headers['X-GitHub-Hook-Installation-Target-Type'] = req.headers[
+				'x-github-hook-installation-target-type'
+			] as string;
+		}
+
+		const discordRes = await fetch(url, { body, headers, method: req.method });
+		const discordHeaders = [...discordRes.headers];
+		res.writeHead(discordRes.status, discordRes.statusText, discordHeaders);
 		discordRes.body?.pipe(res);
 	} catch (err) {
 		respondJSON(res, 500, `Error while forwarding request to discord`, err);
