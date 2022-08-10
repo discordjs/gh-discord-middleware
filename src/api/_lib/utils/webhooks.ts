e94d1596a96ccbc7fa2caf2ab671de0729a5d096
ckohen <chaikohen@gmail.com>
2022-08-09 19:31:18 -0700

    feat: support forums / channels with threads more cleanly

--- src/api/_lib/utils/webhooks.ts
+++ src/api/_lib/utils/webhooks.ts
@@ -1,6 +1,14 @@
 import type { PackageName } from './constants.js';
 
-export const DiscordWebhooks = {
+const useForum = process.env.USE_FORUM === 'true';
+
+const webhookBase = useForum ? process.env.DISCORD_WEBHOOK_FORUM_BASE : '';
+
+if (typeof webhookBase !== 'string') {
+	throw new TypeError('DISCORD_WEBHOOK_FORUM_BASE must be defined to use forums');
+}
+
+const Webhooks = {
 	monorepo: process.env.DISCORD_WEBHOOK_MONOREPO,
 	builders: process.env.DISCORD_WEBHOOK_BUILDERS,
 	collection: process.env.DISCORD_WEBHOOK_COLLECTION,
@@ -11,6 +19,13 @@ export const DiscordWebhooks = {
 	website: process.env.DISCORD_WEBHOOK_WEBSITE,
 	ws: process.env.DISCORD_WEBHOOK_WS,
 } as const;
+
+const ForumWebhooks = Object.entries(Webhooks).reduce(
+	(acc, [pack, val]) => ({ ...acc, [pack]: val ? `${webhookBase}?thread_id=${val}` : undefined }),
+	{},
+) as typeof Webhooks;
+
+export const DiscordWebhooks = useForum ? ForumWebhooks : Webhooks;
 
 export type DiscordWebhooksTarget = keyof typeof DiscordWebhooks | 'none';
 
