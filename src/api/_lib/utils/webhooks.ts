5296298ab2cd9965d9e6be32239aca1abb97bf67
ckohen <chaikohen@gmail.com>
2022-10-07 12:28:55 -0700

    feat: add new and upcoming subpackages (#6)
    * feat: add new and upcoming subpackages
    
    * docs: add new env vars

--- src/api/_lib/utils/webhooks.ts
+++ src/api/_lib/utils/webhooks.ts
@@ -8,13 +8,21 @@ if (typeof webhookBase !== 'string') {
 	throw new TypeError('DISCORD_WEBHOOK_FORUM_BASE must be defined to use forums');
 }
 
+// TODO cleanup, somehow use PackageName to only update one place for new packages
+
 const Webhooks = {
 	monorepo: process.env.DISCORD_WEBHOOK_MONOREPO,
+	brokers: process.env.DISCORD_WEBHOOK_BROKERS,
 	builders: process.env.DISCORD_WEBHOOK_BUILDERS,
 	collection: process.env.DISCORD_WEBHOOK_COLLECTION,
 	'discord.js': process.env.DISCORD_WEBHOOK_DISCORDJS,
+	guide: process.env.DISCORD_WEBHOOK_GUIDE,
 	proxy: process.env.DISCORD_WEBHOOK_PROXY,
 	rest: process.env.DISCORD_WEBHOOK_REST,
+	sharder: process.env.DISCORD_WEBHOOK_SHARDER,
+	structures: process.env.DISCORD_WEBHOOK_STRUCTURES,
+	ui: process.env.DISCORD_WEBHOOK_UI,
+	util: process.env.DISCORD_WEBHOOK_UTIL,
 	voice: process.env.DISCORD_WEBHOOK_VOICE,
 	website: process.env.DISCORD_WEBHOOK_WEBSITE,
 	ws: process.env.DISCORD_WEBHOOK_WS,
@@ -31,14 +39,20 @@ export type DiscordWebhooksTarget = keyof typeof DiscordWebhooks | 'none';
 
 export const PerPackageWebhooks: Record<PackageName, DiscordWebhooksTarget> = {
 	actions: 'monorepo',
+	brokers: 'brokers',
 	builders: 'builders',
 	collection: 'collection',
 	'discord.js': 'discord.js',
 	docgen: 'monorepo',
+	guide: 'guide',
 	proxy: 'proxy',
 	'proxy-container': 'proxy',
 	rest: 'rest',
 	scripts: 'monorepo',
+	sharder: 'sharder',
+	structures: 'structures',
+	ui: 'ui',
+	util: 'util',
 	voice: 'voice',
 	website: 'website',
 	ws: 'ws',
