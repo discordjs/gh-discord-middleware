40e85334f40cddd033b9e1789b8ec299879252b7
Almeida <almeidx@pm.me>
2023-01-13 08:05:32 +0000

    feat: filter comments by github-actions bot (#9)
    * feat: filter pr comments by github-actions bot
    
    * feat: filter commit comments by github-actions bot

--- src/api/_lib/utils/filters.ts
+++ src/api/_lib/utils/filters.ts
@@ -13,6 +13,9 @@ import {
 	DiscardVercelPrComments,
 	DiscardVercelCommitComments,
 	VercelBotId,
+	GithubActionsBotId,
+	DiscardGithubActionsPrComments,
+	DiscardGithubActionsCommitComments,
 } from '../utils/constants.js';
 
 export function filterPrComments(
@@ -26,11 +29,13 @@ export function filterPrComments(
 ): boolean {
 	if (DiscardCodecovComments && event.sender.id === CodecovBotId) return true;
 	if (DiscardVercelPrComments && event.sender.id === VercelBotId) return true;
+	if (DiscardGithubActionsPrComments && event.sender.id === GithubActionsBotId) return true;
 	return false;
 }
 
 export function filterCommitComments(event: CommitCommentEvent): boolean {
 	if (DiscardCodecovComments && event.comment.user.id === CodecovBotId) return true;
 	if (DiscardVercelCommitComments && event.comment.user.id === VercelBotId) return true;
+	if (DiscardGithubActionsCommitComments && event.comment.user.id === GithubActionsBotId) return true;
 	return false;
 }
