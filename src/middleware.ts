b8c3a8c2d0dfaa8d7a3775f13811b7d48d4ee9a6
ckohen <chaikohen@gmail.com>
2024-02-24 19:40:49 -0800

    refactor: no TLA :(

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,7 @@
 import { verify, sign } from '@octokit/webhooks-methods';
 import { get } from '@vercel/edge-config';
 
-const debugLog = await get<boolean | undefined>('debugLogs');
-
-function log(...messages: unknown[]) {
+async function log(debugLog: boolean, ...messages: unknown[]) {
 	if (debugLog) {
 		console.log(...messages);
 	}
@@ -15,21 +13,22 @@ const continueHeaders = new Headers();
 continueHeaders.set('X-Middleware-Next', '1');
 
 export default async function middleware(request: Request) {
+	const debugLog = (await get<boolean | undefined>('debugLogs')) ?? false;
 	if (!secret) {
-		log('No secret set, continuing');
+		await log(debugLog, 'No secret set, continuing');
 		return new Response(null, { headers: continueHeaders });
 	}
 
 	const githubSignature = request.headers.get('X-Hub-Signature-256');
 	if (!githubSignature) {
-		log('Secret set, no signature header');
+		await log(debugLog, 'Secret set, no signature header');
 		return new Response(null, { status: 401, statusText: 'Signature Missing' });
 	}
 
 	const payload = await request.text();
 	const verified = await verify(secret, payload, githubSignature);
 	if (!verified) {
-		log('Verification failed. Local:', await sign(secret, payload), 'Received:', githubSignature);
+		await log(debugLog, 'Verification failed. Local:', await sign(secret, payload), 'Received:', githubSignature);
 		return new Response(null, { status: 401 });
 	}
 
